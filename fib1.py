d7f8697be8ad2cb2f598e04fe7835b4c868cc38b
Ian <ian.christopher.mason@gmail.com>
2021-02-17 09:07:43 -0600

    first commit

---
+++ fib1.py
@@ -0,0 +1,9 @@
+def fib1(n: int) -> int:
+	if n < 2:
+		return n
+	return fib1(n - 1) + fib1(n - 2)
+
+
+if __name__ == "__main__":
+    print(fib1(5))
+# Note that this example is purposefully wrong.
