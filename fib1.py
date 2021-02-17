20654cb29c310fe420eb827856cf2c2fb8494b71
Ian <ian.christopher.mason@gmail.com>
2021-02-17 14:47:49 -0600

    memoization

--- fib1.py
+++ fib1.py
@@ -1,9 +1,13 @@
+from typing import Dict
+memo: Dict[int, int] = {0: 0, 1: 1}
+
 def fib1(n: int) -> int:
-	if n < 2:
-		return n
-	return fib1(n - 1) + fib1(n - 2)
+	if n not in memo:
+		memo[n] = fib1(n-1) + fib1(n-2)
+	return memo[n]
 
 
 if __name__ == "__main__":
     print(fib1(5))
+    print(fib1(50))
 # Note that this example is purposefully wrong.
